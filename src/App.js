9f51a6ff36098d90905853461e90e5c403413238
Matt Werth <matthewwerth@gmail.com>
2021-06-08 16:02:09 -0400

    Added Basic Set-up For Amplify Accounts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports'
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react'
+import awsmobile from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>You DID IT! You created a USER ACCOUNT!!!</h1>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
